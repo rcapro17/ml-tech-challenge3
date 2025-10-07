834eac1db4f2f2f9cfbd9250009c9b26889ddbc5
rodrigo <rca17@me.com>
2025-10-06 21:00:24 -0300

    imports relativos atualizados

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,5 @@
-# wsgi.py (na raiz do repo)
 import os, sys
 BASE_DIR = os.path.dirname(__file__)
-# garante que a raiz do projeto esteja no sys.path
 if BASE_DIR not in sys.path:
     sys.path.insert(0, BASE_DIR)
 
